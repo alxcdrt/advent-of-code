e8676837a742eab8f1d3c2a11520ab1de10c07bf
Alexis Coudert <coudert.alex@gmail.com>
2021-12-03 14:40:46 +0100

    2021 - d3p1

---
+++ lib/2021/day_3.ex
@@ -0,0 +1,31 @@
+defmodule AdventOfCode.Year2021.Day3 do
+
+
+  def part_1(input) do
+    parse_input(input)
+      |> Enum.map(&(String.graphemes(&1)))
+      |> Enum.zip_with(&(get_most_common_bit(&1)))
+      |> Enum.join()
+      |> IO.inspect()
+      |> compute_power_consuption()
+  end
+
+
+  def part_2(input) do
+
+  end
+
+  defp parse_input(input), do: input |> String.split("\n")
+
+  defp get_most_common_bit(enum) do
+    {bit, _} = Enum.frequencies(enum) |> Enum.max_by(fn {_, v} -> v end)
+    bit
+  end
+
+  defp compute_power_consuption(gamma) do
+    b_gamma = String.to_integer(gamma, 2)
+    max = :math.pow(2, String.length(gamma)) - 1 |> trunc()
+
+    b_gamma * Bitwise.bxor(b_gamma, max)
+  end
+end
---
+++ test/2021/day_3_test.exs
@@ -0,0 +1,20 @@
+defmodule Day3Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Year2021.Day3
+
+  setup_all do
+    root_path = File.cwd!
+    input = File.read!(root_path <> "/test/input/2021/day_3")
+    {:ok, input: input}
+  end
+
+  test "part_1", context do
+    assert(part_1(context[:input]) == 198)
+  end
+
+  test "part_2", context do
+
+  end
+
+end
